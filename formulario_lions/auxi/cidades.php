eecce7e037a1499edf8c4543181b20c6df0fc66c
rob.engcomp <rob.engcomp@gmail.com>
2014-01-14 03:30:19 +0000

    Adicionar Todos Arquivos

---
+++ formulario_lions/auxi/cidades.php
@@ -0,0 +1,62 @@
+﻿<?php 
+extract ($_POST);
+
+if($estado == "PARÁ"){
+$resposta = "
+	<option class='para' value='' selected></option>
+	<option class='para' value='ABAETETUBA'>ABAETETUBA</option>
+	<option class='para' value='ALTAMIRA'>ALTAMIRA</option>
+	<option class='para' value='BELÉM'>BELÉM</option>
+	<option class='para' value='BENEVIDES'>BENEVIDES</option>
+	<option class='para' value='BRAGANÇA'>BRAGANÇA</option>
+	<option class='para' value='BREU BRANCO'>BREU BRANCO</option>
+	<option class='para' value='CASTANHAL'>CASTANHAL</option>
+	<option class='para' value='CAPITÃO POÇO'>CAPITÃO POÇO</option>
+	<option class='para' value='DOM ELISEU'>DOM ELISEU</option>
+	<option class='para' value='ELDORADO DO CARAJÁS'>ELDORADO DO CARAJÁS</option>
+	<option class='para' value='ICOARACI'>ICOARACI</option>
+	<option class='para' value='ITAITUBA'>ITAITUBA</option>
+	<option class='para' value='IPIXUNA DO PARÁ'>IPIXUNA DO PARÁ</option>
+	<option class='para' value='ITUPIRANGA'>ITUPIRANGA</option>
+	<option class='para' value='JACUNDÁ'>JACUNDÁ</option>
+	<option class='para' value='MARABÁ'>MARABÁ</option>
+	<option class='para' value='MOJU'>MOJU</option>
+	<option class='para' value='NOVO PROGRESSO'>NOVO PROGRESSO</option>
+	<option class='para' value='PARAGOMINAS'>PARAGOMINAS</option>
+	<option class='para' value='PARAUAPEBAS'>PARAUAPEBAS</option>
+	<option class='para' value='RONDON DO PARÁ'>RONDON DO PARÁ</option>
+	<option class='para' value='TAILÂNDIA'>TAILÂNDIA</option>
+	<option class='para' value='TUCURUÍ'>TUCURUÍ</option> ";
+}
+
+if($estado == "MARANHÃO"){
+$resposta = "
+	<option class='maranhao' value='' selected></option>
+	<option class='maranhao' value='CAXIAS'>CAXIAS</option>
+	<option class='maranhao' value='CODÓ'>CODÓ</option>
+	<option class='maranhao' value='SANTA INÊS'>SANTA INÊS</option>
+	<option class='maranhao' value='SANTA LUZIA'>SANTA LUZIA</option>
+	<option class='maranhao' value='SÃO LUIZ'>SÃO LUIZ</option>
+	<option class='maranhao' value='TIMON'>TIMON</option> ";
+}
+
+if($estado == "PIAUÍ"){
+$resposta = "	
+	<option class='piaui' value='' selected></option>
+	<option class='piaui' value='CAMPO MAIOR'>CAMPO MAIOR</option>
+	<option class='piaui' value='FLORIANO'>FLORIANO</option>
+	<option class='piaui' value='LUÍS CORREIA'>LUÍS CORREIA</option>
+	<option class='piaui' value='PARNAIBA'>PARNAIBA</option>
+	<option class='piaui' value='PICOS'>PICOS</option>
+	<option class='piaui' value='TERESINA '>TERESINA</option> ";
+}
+
+if($estado == "AMAPÁ"){
+$resposta = "	
+	<option class='amapa' value='' selected></option>
+	<option class='amapa' value='MACAPÁ'>MACAPÁ</option> ";
+}
+
+echo $resposta;
+
+?>
---
+++ formulario_lions/auxi/salvar.php
@@ -0,0 +1,36 @@
+﻿<?php 
+	include "conn.php";
+	$conecta = new Conecta();
+	
+	if(!$_POST){
+		header('Location: http://www.lionsdla6.com.br');
+	}
+		
+	extract ($_POST);
+	
+	$nascimento = "{$ano}-{$mes}-{$dia}";
+	
+	if($matricula == NULL){ $matricula = 'NULL'; }	
+	if($dia==NULL && $mes==NULL && $ano==NULL){ $nascimento = 'NULL';} 
+	if($endereco == NULL){ $endereco = 'NULL'; }
+	if($complemento == NULL){ $complemento = 'NULL'; }	
+	if($cargo_clube == NULL){ $cargo_clube = 'NULL'; }
+	if($outro_cargo_clube == NULL){ $outro_cargo_clube = 'NULL'; }
+	if($cargo_distrito == NULL){ $cargo_distrito = 'NULL'; }
+	if($outro_cargo_distrtito == NULL){ $outro_cargo_distrtito = 'NULL'; }
+	
+
+	$sql = "INSERT INTO xv_convencao (nome, matricula, cpf, email, nascimento, endereco, complemento, estado, cidade, clube, delegado, cargo_clube, qual_cc, cargo_distrito, qual_cd, cl_mj, prefixo, camisa ) VALUES ('$nome', '$matricula', '$cpf', '$email', '$nascimento', '$endereco', '$complemento', '$estado', '$cidade', '$clube', '$delegado', '$cargo_clube', '$outro_cargo_clube', '$cargo_distrito', '$outro_cargo_distrtito', '$cl_mj', '$prefixo', '$camisa' )";
+	
+	$query = mysql_query($sql);
+	
+	
+	if(mysql_error($conecta->conn)){
+		echo mysql_error($conecta->conn);
+	}
+	else{
+		include "sucesso.html";	
+	}
+	$conecta->desconecta();
+	
+?>
