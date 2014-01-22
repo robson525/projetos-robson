988ab8a5f64d31052b49c50ef8bac4b626112036
rob.engcomp@gmail.com <rob.engcomp@gmail.com@5fd99128-cd4d-e4e5-e074-a8a5b56f6fab>
2014-01-22 15:36:36 +0000

    Iniciando

---
+++ trunk/paiamab/index.php
@@ -0,0 +1,15 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sem título</title>
+</head>
+
+<body>
+<?php 
+
+include "login.php";
+
+?>
+</body>
+</html>
---
+++ trunk/paiamab/login.php
@@ -0,0 +1,18 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sem título</title>
+<link href="css/login.css" rel="stylesheet" type="text/css"/>
+
+</head>
+
+
+<body>
+
+<div id="div_login">
+	<h1>OI</h1>
+</div>
+
+</body>
+</html>
