c7c00f0439e170bc6040d170b203b73587fe871d
rob.engcomp@gmail.com <rob.engcomp@gmail.com@5fd99128-cd4d-e4e5-e074-a8a5b56f6fab>
2014-02-01 16:58:29 +0000

    Pasta auxiliar com o arquivo que valida login do usuario

---
+++ trunk/paiamab/auxiliar/validaLogin.php
@@ -0,0 +1,26 @@
+﻿<?php 
+	if(!$_POST || !$_POST["login"] || !$_POST["senha"]){
+		echo "Acesso Negado";
+		echo "<meta charset='utf-8' http-equiv='refresh' content='1; url=index.php'>";
+		exit();
+	}
+	
+	require_once ('../classes/class.usuario.php');
+	extract($_POST);
+	
+	$usuario = new Usuario();
+	$existe = $usuario->validaUsuario($login, $senha);
+	
+	if($existe){
+		session_start();
+		$_SESSION['id_usuario'] = $usuario->get_id();
+		echo false;	
+	}
+	else{
+		echo "Login Inválido";	
+	}
+	
+	
+
+
+?>
