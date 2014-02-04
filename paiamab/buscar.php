8ddec33d3ff0bef0b7e0a92771c715542070afff
rob.engcomp <rob.engcomp@gmail.com>
2014-02-04 05:12:53 +0000

    Div Buscar

---
+++ paiamab/buscar.php
@@ -0,0 +1,28 @@
+﻿<?php 
+		//Verifica se os Dados de busca Estão Preenchidos
+
+		require_once "../../class/usuario.class.php";
+		
+		$nome = $_POST["nome"];
+		$cpf = $_POST["cpf"];
+		$login = $_POST["login"];
+		
+		$usuario = new Usuario();
+		$sql = $usuario->busca($nome, $login, $cpf);
+		$aux = $usuario->verificaResultado($sql);
+		
+	
+		if($nome==NULL && $cpf==NULL && $login==NULL){
+			echo '<script type="text/javascript">alert("Preencha Pelo Menos um dos Campos");</script>';
+		}
+		else if($aux == false){
+			echo '<script type="text/javascript">alert("Nenhum Resultado Encontrado");</script>';	
+		}
+		else{
+			echo false;
+		}
+		
+?>
+
+
+
