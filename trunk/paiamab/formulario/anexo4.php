753a1ba78fb90c1eaed11ee9d2189410e3b2dc4f
enilsonst@gmail.com <enilsonst@gmail.com@5fd99128-cd4d-e4e5-e074-a8a5b56f6fab>
2014-02-04 03:39:55 +0000

    atualização anexo4

--- trunk/paiamab/formulario/anexo4.php
+++ trunk/paiamab/formulario/anexo4.php
@@ -2,9 +2,300 @@
 <html>
 <head>
 <meta charset="utf-8">
+<link href="css/formularios.css" rel="stylesheet" type="text/css"/>
 <title>Documento sem título</title>
 </head>
-<h1>Anexo 4 </h1>
+<center>
+<h1>Anexo 4: Doenças de Pele</h1>
+</center>
 <body>
+
+<div id="div_anexos">
+
+
+<form id="anexo4">
+	   
+ <table id="tab1" border="1"  cellspacing="0" >  
+ 
+ 
+        <tr>
+        	<td >1 - Você já teve alguma doença de pele?  </td>
+            <td >	<select id="tab1_p1" name="tab1_p1">
+                                <option value=""></option>
+                                <option value="sim">Sim</option>
+                                <option value="não">Não</option>
+                                </select>
+              &ensp;&ensp;
+        	<span id="tab1_p1.1"> Qual:  </span> <input id="tab1_p1.2" name="tab1_p1.2" type="text" maxlength="20"></td>
+        
+        </tr>
+               
+       
+       
+       <tr>
+          <td>2 - Você tem alguma macha na pele (dói,coça, muda de cor)?</td>
+          <td>
+                <select id="tab1_p2" name="tab1_p2">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>
+                </select><span id="tab1_p2.1">Reação: 
+                <input id="tab1_p2.1" name="tab1_p2.1" type="text" maxlength="20"> </span> </td>
+       </tr>
+       
+       <tr>
+          <td>3 - Em sua casa alguém já teve ou tem alguma doença de pele?</td>
+          <td>
+                <select id="tab1_p3" name="tab1_p3">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>
+                 &ensp;&ensp;
+        	<span id="tab1_p3.1"> Qual:  </span> <input id="tab1_p3.2" name="tab1_p3.2" type="text" maxlength="20"></td>
+       </tr>
+       
+       <tr>
+          <td>4 - Você tem alguma caroço no corpo ou na cabeça?</td>
+          <td>
+                <select id="tab1_p4" name="tab1_p4">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>&ensp;&ensp;
+        	<span id="tab1_p4.1">Local: <input id="tab1_p4.1" name="tab1_p4.1" type="text" maxlength="20"> </span> </td>
+       </tr>
+       
+        <tr>
+          <td>5 - Tem alguma parte de seu corpo onde coça (pode ser na cabeça ou nas unhas)?</td>
+          <td>
+                <select id="tab1_p5" name="tab1_p5">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>&ensp;&ensp;
+        	<span id="tab1_p5.1">Local: <input id="tab1_p5.1" name="tab1_p5.1" type="text" maxlength="20"> </span> </td>
+       </tr>
+       
+       <tr>
+          <td>6 - Em caso de feridas (coça, tem pus, dói, quanto tempo, usa algum remédio para tratar)?</td>
+          <td>                
+        	<span id="tab1_p6">&ensp;Reação<input id="tab1_p6" name="tab1_p6" type="text" maxlength="20"> </span>
+            &ensp;&ensp;
+        	<span id="tab1_p6.1">Tempo<input id="tab1_p6.1" name="tab1_p6.1" type="text" maxlength="20"> </span>
+            &ensp;&ensp;
+        	<span id="tab1_p6.2">Remédio<select id="tab1_p6.2" name="tab1_p6.2">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>&ensp;&ensp; </td> </td> </td>
+       </tr>
+       
+       <tr>
+          <td>7 - Você toma banho todos os dias?</td>
+          <td>
+                <select id="tab1_p7" name="tab1_p7">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select> &ensp;&ensp;
+                <span>Quantos dias:</span>
+                <select id="tab1_p7.1" name="tab1_p7.1">
+                <option value=""></option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                </select>
+       </tr>
+       
+       <tr>
+          <td>8 - Durante o banho o que você costuma usar no corpo (sabão,sabonete)?</td>
+          <td>
+                <select id="tab1_p8" name="tab1_p8">
+                <option value=""></option>
+                <option value="sabao">Sabão</option>
+                <option value="sabonete">Sabonete</option>
+                <option value="outros produtos">Outros Produtos</option>
+                </select>
+                
+                
+       </tr>
+       
+       <tr>
+          <td>9 - Você lava os cabelos todos os dias (usa shapoo, condicionador, sabão, sabonete)?</td>
+          <td>
+                <select id="tab1_p9" name="tab1_p9">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>&ensp;&ensp;
+                <select id="tab1_p9.1" name="tab1_p9.1">
+                <option value=""></option>
+                <option value="Shapoo">Shapoo</option>
+                <option value="Condicionador">Condicionador</option>
+                <option value="Sabão">Sabão</option>
+                <option value="Sabonete">Sabonete</option>
+                <option value="Outros Produtos">Outros Produtos</option>                
+                </select>
+       </tr>
+       
+        <tr>
+          <td>10 - Voçe usa protetor solar?</td>
+          <td>
+                <select id="tab1_p10" name="tab1_p10">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>
+       </tr>
+       
+       <tr>
+          <td>11 - Costuma usar chapéu, guarda-sol, algo para se proteger do sol?</td>
+          <td>
+                <select id="tab1_p11" name="tab1_p11">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>
+                <span id="tab1_p11.1">Qual tipo de produto ou objeto: 
+                <input id="tab1_p11.1" name="tab1_p11.1" type="text" maxlength="20"> </span> </td>
+       </tr>
+       
+       <tr>
+          <td>12 -  Divide roupas com outras pessoas?</td>
+          <td>
+                <select id="tab1_p12" name="tab1_p12">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>
+       </tr>
+            
+   </table>            
+     
+          
+   <center>
+   <h1>Dados Complementares:</h1>
+   </center>
+<table id="tab2" border="1" align="center" cellspacing="0" >
+     
+        <tr>
+          <td>1 -  Você morra na área urbana ou rural?</td>
+          <td>
+                <select id="tab2_p1" name="tab2_p1">
+                <option value=""></option>
+                <option value="Urbana">Urbana</option>
+                <option value="Rural">Rural</option>
+                </select>
+       </tr>
+       
+       <tr>
+          <td>2 - Como é sua casa?</td>
+          <td>
+                <select id="tab2_p2" name="tab2_p2">
+                <option value=""></option>
+                <option value="Alvenaria">Alvenaria</option>
+                <option value="Edifício">Edifícios</option>
+                <option value="Madeira">Madeira</option>
+                <option value="Palafita">Palafita</option>                
+                <option value="Casa de pau a pique">Casa de pau a pique</option>
+                <option value="Barraca de Lona">Barraca de Lona</option>
+                
+                
+                </select>
+       </tr>
+       
+        <tr>
+          <td>3 - Qual é sua profissal?</td>
+          <td>             
+              <input id="tab2_p3" name="tab2_p3" type="text" maxlength="20"> </span>
+       </tr>   
+           
+  </table>   
+     
+     
+     
+     
+   <center>
+   <h1>Após o exame físico:</h1> 
+   </center>
+      
+   <table id="tab3" border="1" align="center" cellspacing="0"   
+       <tr>
+       <td >1 - Encontrou mancha no corpo? </td>
+            <td >
+                <select id="tab3_p1" name="tab3_p1">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>
+       </tr>
+       
+       <td >2 - Qual a Cor? </td>
+            <td >
+                 <input id="tab3_p2" name="tab3_p2" type="text" maxlength="20"> </span> </td>
+       </tr>  
+       
+       <td >3 - Qual o Aspecto? </td>
+            <td  >
+                 <input id="tab3_p3" name="tab3_p3" type="text" maxlength="20"> </span> </td>
+       </tr>
+       
+       <td >4 - Qual o Diametro? </td>
+            <td >
+               <input id="tab3_p4" name="tab3_p4" type="text" maxlength="20"> </span> </td>
+       </tr>
+       
+       <td>5 - Descamação: </td>
+            <td >
+                <select id="tab3_p5" name="tab3_p5">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>
+       </tr>
+       
+       <td>6 - Coça: </td>
+            <td >
+                <select id="tab3_p6" name="tab3_p6">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>
+       </tr>
+       
+       <td>7 - Nas unhas apresenta fungos? </td>
+            <td >
+                <select id="tab3_p7" name="tab3_p7">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>
+       </tr>
+       
+       <td>8 - Na cabeça encontrou fungos? </td>
+            <td >
+                <select id="tab3_p8" name="tab3_p8">
+                <option value=""></option>
+                <option value="sim">Sim</option>
+                <option value="não">Não</option>
+                </select>
+       </tr>
+     
+            
+   <table>  
+                      <br/ >
+   
+                     <input id="submit" name="submit" type="submit" value="Salvar"  </td>     
+     
+</form>
+
+
+</div>
+
 </body>
 </html>
